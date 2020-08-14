29fcea846aeab403453cfd3804936f8055f76b10
Nacho <ignacioga.346@gmail.com>
2020-08-14 04:24:33 -0300

    Submit project 1

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "ency"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:entry>", views.page, name="page")
+    path("wiki/<str:entry>", views.page, name="page"),
+    path("search", views.search, name="search"),
+    path("Rpage", views.Rpage, name="Rpage"),
+    path("create", views.create, name="create"),
+    path("edit/<str:entry>", views.edit, name="edit")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,9 +1,18 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
-
+import random
 from . import util
+from django.contrib import messages
+import markdown2
+from django.utils.safestring import mark_safe
 
+
+def buscar(x):
+	listSubEntry = []
+	entries = util.list_entries()
+	for entry in entries:		
+		if x in entry:
+			listSubEntry.append(entry)
+	return listSubEntry
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -14,7 +23,64 @@ def page(request, entry):
 	if (util.get_entry(entry)!=None):
 		return render(request, "encyclopedia/page.html", {
         "entry": entry.capitalize(),
-        "content": util.get_entry(entry)
-        	})      
+        "content": markdown2.markdown(util.get_entry(entry))
+        	})       	      
 	else:
-		return HttpResponseRedirect(reverse("index"))
+		return render(request, "encyclopedia/error.html")
+
+def search(request):
+	entry = request.GET.get('q')
+	print(entry)	
+	if(util.get_entry(entry)!=None):
+		return render(request, "encyclopedia/page.html", {
+        "entry": entry.capitalize(),
+        "content": markdown2.markdown(util.get_entry(entry))
+        	})
+	else:
+		lista = buscar(entry)
+		return render(request, "encyclopedia/search.html", {
+		"entry": entry,
+        "result": lista
+        	})
+
+def Rpage(request):
+	entries = util.list_entries()
+	page = random.choice(entries)
+	return render(request, "encyclopedia/page.html", {
+        "entry": page.capitalize(),
+        "content": markdown2.markdown(util.get_entry(page))
+        	}) 
+
+def create(request):
+	entries = util.list_entries()
+	if request.method == "POST":
+		entry = request.POST["title"]
+		content = request.POST["content"]	
+		if entry in entries:
+			messages.error(request, mark_safe("This entry already exists. <a href='/wiki/"+entry+"'>"+entry+" entry </a>"))
+			return render(request, "encyclopedia/create.html",{
+	   			"entry": entry,
+	   		    "content": content
+	   				})
+		else:
+			util.save_entry(entry, content)
+			return render(request, "encyclopedia/page.html", {
+        		"entry": entry.capitalize(),
+        		"content": markdown2.markdown(util.get_entry(entry))
+        			}) 
+	else:
+		return render(request, "encyclopedia/create.html")
+
+def edit(request, entry):
+	if request.method == "POST":
+		text = request.POST["editable"]
+		util.save_entry(entry, text)
+		return render(request, "encyclopedia/page.html", {
+        		"entry": entry.capitalize(),
+        		"content": markdown2.markdown(util.get_entry(entry))
+        			})
+	else:
+		return render(request, "encyclopedia/edit.html",{
+			"entry": entry.capitalize(),
+        	"txtedit": util.get_entry(entry)
+				})
